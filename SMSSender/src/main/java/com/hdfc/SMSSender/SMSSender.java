c1d23b940c07ccffcb9189b27cddad43dd5992f1
amuthapriya4 <amuthapriya4@gmail.com>
2022-05-06 14:35:56 +0530

    Initial Commit

---
+++ SMSSender/src/main/java/com/hdfc/SMSSender/SMSSender.java
@@ -0,0 +1,44 @@
+package com.hdfc.SMSSender;
+
+import java.util.Map;
+import hdfcbank.ws.smssender.SMSResponse;
+
+
+public class SMSSender {
+	
+	static SMSResponse response=new SMSResponse();
+	
+ public static SMSResponse sendSMS() {
+	 Map<String, String> mapResponse= ResponseStatusLoaderController.mapResponse;
+		
+	 response.setFiller1(""); response.setFiller2("");response.setFiller3("");
+	 response.setFiller4("");response.setFiller5("");
+	 
+	 response.setResponsecode("0");
+	 
+	 if(mapResponse.isEmpty() ) {		
+		 response.setResponsecode("0");  
+    	 response.setMessage("Success");    	 
+	 }else if (mapResponse.get("status").contentEquals("success")) {
+		 response.setResponsecode(mapResponse.get("responseCode"));  
+    	 response.setMessage(mapResponse.get("responseDesc"));
+	 }else if (mapResponse.get("status").contentEquals("failure")) {
+		 response.setResponsecode(mapResponse.get("responseCode"));  
+    	 response.setMessage(mapResponse.get("responseDesc")); 
+	 }else if (mapResponse.get("status").contentEquals("exception")) {
+		 throw new RuntimeException();
+	 }else if (mapResponse.get("status").contentEquals("timeout")) {
+		 try {
+				Thread.sleep(1800000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	 }else {
+		 response.setResponsecode("-1");  
+    	 response.setMessage("Error");  
+	 }
+	return response;
+	 
+ }
+}
